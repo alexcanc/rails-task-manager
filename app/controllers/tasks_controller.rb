ce249d02f1253da637180d3d74462c17cbbe765f
Alexandre Canacaris <tiloucanac1300@gmail.com>
2023-08-04 12:24:40 +0200

    Added the ability to create a new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,6 +11,19 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to task_path(@task)
+    else
+      render :edit
+    end
+  end
+
   def create
     @task = Task.new(task_params)
     if @task.save
@@ -20,10 +33,15 @@ class TasksController < ApplicationController
     end
   end
 
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
   private
 
   def task_params
     params.require(:task).permit(:title, :details)
   end
-
 end
